5a3f2c0cfa63371188a95e42fb63c1cc14f5a7a2
Litoru <puwarin2001@gmail.com>
2021-03-04 15:54:56 +0700

    ทิ้ง 4_2 ไว้ก่อน

--- 6320500620_4_2.c
+++ 6320500620_4_2.c
@@ -2,5 +2,20 @@
 
 void main()
 {
+    int n,i,k,get;
+    scanf("%d",&n);
+    char term[n];
+
+    fflush(stdin);
+    for(i=0 ; i<n ; i++)
+    {
+        scanf("%[^" "]c",&term[i]);
+    }
+
+
+    for(i=0 ; i<n ; i++)
+        printf("%c ",term[i]);
 
 }
+
+
