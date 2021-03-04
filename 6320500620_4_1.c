37b64f3f50edcf67e3f21bab6228685d9d13828d
Litoru <puwarin2001@gmail.com>
2021-03-04 15:16:11 +0700

    code ดูน้ำหนักเหรียญ

--- 6320500620_4_1.c
+++ 6320500620_4_1.c
@@ -10,26 +10,53 @@ void main()
 
     for(i=0 ; i<n ; i++)
     {
-        for(i=0 ; k<3 ; k++)
+        for(k=0 ; k<3 ; k++)
         {
             scanf("%d",&get);
             if(k==0)
-                score[k]=score[k]+(get*4);
+                score[i]=score[i]+(get*4);
             else if(k==1)
-                score[k]=score[k]+(get*2);
+                score[i]=score[i]+(get*2);
             else if(k==2)
-                score[k]=score[k]+(get*1);
+                score[i]=score[i]+(get*1);
             else
                 printf("\n\n\nLoop score Error\n\n\n");
         }
     }
-    for(i=0 ; i<n ; )
+    int save[n],max=0,min=0;
+
+    for(i=0 ; i<n ; i++)
+        save[i]=0;
+
+    for(i=0 ; i<n ; i++)
     {
         for(k=0 ; k<3 ; k++)
         {
-
+            if(i <= n-3)
+            {
+                if(k+i < n)
+                    save[i]=save[i]+score[i+k];
+            }
+            else
+            {
+                save[i]=save[i-1];
+            }
+        }
+        if(save[max]<save[i])
+        {
+            max=i;
+        }
+        else if(save[min]>save[i])
+        {
+            min=i;
         }
     }
+    printf("%d %d",max+1,min+1);
 
-
+    /*
+    for(i=0 ; i<n ; i++)
+        printf("score = %d\n",score[i]);
+    for(i=0 ; i<n ; i++)
+        printf("save = %d\n",save[i]);
+    */
 }
