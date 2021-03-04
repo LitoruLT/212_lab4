3433bf80e74c4f06b0786e03c603fc4b80593534
Litoru <puwarin2001@gmail.com>
2021-03-04 16:12:37 +0700

    Create 6320500620_4_4.c

---
+++ 6320500620_4_4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+    int n=200,i,k=0;
+    char term[n],ans[n];
+
+    scanf("%[^\n]",term);
+
+    for(i=0; i<n ;i++)
+    {
+        if(term[i]!=term[i+1])
+        {
+
+        }
+    }
+
+
+}
+
+
