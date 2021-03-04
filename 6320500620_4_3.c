aa6d14ee7915a1ed9777c37ff2b9c2486afe0dcf
Litoru <puwarin2001@gmail.com>
2021-03-04 16:04:05 +0700

    4_3 done

--- 6320500620_4_3.c
+++ 6320500620_4_3.c
@@ -2,6 +2,22 @@
 
 void main()
 {
+    int n=200,i,k=0;
+    char term[n],ans[n];
+
+    gets(term);
+
+    for(i=0; i<n ;i++)
+    {
+        if(term[i]!=term[i+1])
+        {
+            ans[k]=term[i];
+            k++;
+        }
+    }
+
+
+    puts(ans);
 
 
 }
