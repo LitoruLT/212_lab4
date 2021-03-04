d9c3ed9350bda9fa6909e3eaba4b0f59eb74f1ab
Litoru <puwarin2001@gmail.com>
2021-03-04 16:40:13 +0700

    -

--- 6320500620_4_4.c
+++ 6320500620_4_4.c
@@ -2,19 +2,47 @@
 
 void main()
 {
-    int n=200,i,k=0;
-    char term[n],ans[n];
+    int n=200,i=0,k=0,j=0,q;
+    char term[n],ans[n],word[5];
 
     scanf("%[^\n]",term);
 
-    for(i=0; i<n ;i++)
+    ans[j]=term[i];
+    j++;
+
+    for(i=0; term[i]!='\0' ;i++)
     {
-        if(term[i]!=term[i+1])
+        if(term[i-1]==' ')
         {
+            k=0;
+            for(q; q<5 ;q++)
+            {
+                word[q]='\0';
+            }
+            if(word == "i" || word == "of" || word == "the" || word == "on" || word == "at" || word == "for" || word == "with" || word == "a" || word == "in" || word == "an" || word == "and")
+                {
+
+                }
+            else
+            {
+                ans[j]=term[i];
+                j++;
+            }
 
         }
+        else
+        {
+            word[k]=term[i];
+        }
+
     }
 
+    i=0;
+    while( ans[i] != '\0')
+    {
+        printf("%c",ans[i]);
+        i++;
+    }
 
 }
 
